3bb2dc285e16d2f4f2c160fe89bf882bedd2a02f
Luis Garcia <luis.garcia@ucb.edu.bo>
2026-09-06 23:27:49 -0400

    filtros de ejercio

--- app/Http/Controllers/EjercicioController.php
+++ app/Http/Controllers/EjercicioController.php
@@ -7,9 +7,15 @@ use Illuminate\Http\Request;
 
 class EjercicioController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return response()->json(Ejercicio::paginate(10));
+        $query = Ejercicio::query();
+
+        if ($request->has('search')) {
+            $query->where('nombre', 'like', '%' . $request->search . '%');
+        }
+
+        return response()->json($query->paginate(10));
     }
 
     public function store(Request $request)
